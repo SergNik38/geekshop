726e0034fb79bf8c2d6a6f619ec5e614c32f42d8
django <django@185-46-10-235.cloudvps.regruhosting.ru>
2021-12-19 04:57:58 +0300

    lesson_16. F objects

--- basketapp/views.py
+++ basketapp/views.py
@@ -12,9 +12,19 @@ from mainapp.models import Product
 @login_required
 def basket(request):
     title = "корзина"
-    basket_items = Basket.objects.filter(user=request.user).order_by("product__category")
-    content = {"title": title, "basket_items": basket_items, "media_url": settings.MEDIA_URL}
+    basket_items = Basket.objects.filter(user=request.user).order_by(
+        "product__category"
+    )
+    content = {
+        "title": title,
+        "basket_items": basket_items,
+        "media_url": settings.MEDIA_URL,
+    }
     return render(request, "basketapp/basket.html", content)
+
+
+from django.db import connection
+from django.db.models import F
 
 
 @login_required
@@ -26,9 +36,14 @@ def basket_add(request, pk):
 
     if not basket:
         basket = Basket(user=request.user, product=product)
+        basket.quantity += 1
+    else:
+        basket.quantity = F("quantity") + 1
 
-    basket.quantity += 1
     basket.save()
+
+    update_queries = list(filter(lambda x: "UPDATE" in x["sql"], connection.queries))
+    print(f"query basket_add: {update_queries}")
 
     return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
 
@@ -57,7 +72,9 @@ def basket_edit(request, pk, quantity):
         else:
             new_basket_item.delete()
 
-        basket_items = Basket.objects.filter(user=request.user).order_by("product__category")
+        basket_items = Basket.objects.filter(user=request.user).order_by(
+            "product__category"
+        )
 
         content = {"basket_items": basket_items, "media_url": settings.MEDIA_URL}
 
